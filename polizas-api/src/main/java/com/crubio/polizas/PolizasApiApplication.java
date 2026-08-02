e83631eccbf8e8b2be13019cdbbcd7ef66ff8383
Cristian Rubio <cristianrubioa@gmail.com>
2026-08-02 16:36:11 -0500

    :tada: [infra] Scaffold polizas-api Spring Boot project

---
+++ polizas-api/src/main/java/com/crubio/polizas/PolizasApiApplication.java
@@ -0,0 +1,12 @@
+package com.crubio.polizas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolizasApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PolizasApiApplication.class, args);
+  }
+}
